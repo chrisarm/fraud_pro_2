002a0ddf97675c97b46576a4c54b512012dd80f2
Bryce Charette <bryce.charette@gmail.com>
2017-08-01 15:54:40 -0700

    updated hyperparameters

--- scripts/boostedTrees.R
+++ scripts/boostedTrees.R
@@ -8,17 +8,17 @@ train_dat <- readRDS("data/train.RDS")
 set.seed(1234)
 
 # Adjust data type for fraud variable
-train_dat$fraud <- as.numeric(train_dat$fraud)
+# train_dat$fraud <- as.numeric(train_dat$fraud)
 
 # Train the model on the training set
-my.grid <- expand.grid(.interaction.depth = c(1,2), n.trees = c(2000,3000,4000))
+my.grid <- expand.grid(.interaction.depth = c(1,2,3), n.trees = c(2000,3000,4000))
 my.control <- trainControl(method = 'cv', number = 10)
 
 bt.fit <- train(fraud ~ e.1meanamount_zip3 + e.7medianamount_zip3 + e.1medianamount_zip3 + e.7meanamount_cardnum +
                     e.7avgdailyfreq_cardnum + e.7meanamount_merchnum + e.7avgdailyfreq_merchnum + e.1countzip3_cardnum +
                     e.curdaybal_dow_cardnum + e.curdaybal_dow_merchnum,
                   data = train_dat,
-                  method = "bstTree",
+                  method = "gbm",
                   trControl = my.control,
                   tuneGrid = my.grid)
 
