cb2d7642793b570d9e459e36b0fbe65e71f1d20a
Bryce Charette <bryce.charette@gmail.com>
2017-08-01 16:23:20 -0700

    Modified, still working on it

--- scripts/boostedTrees.R
+++ scripts/boostedTrees.R
@@ -8,29 +8,26 @@ train_dat <- readRDS("data/train.RDS")
 set.seed(1234)
 
 # Adjust data type for fraud variable
-# train_dat$fraud <- as.numeric(train_dat$fraud)
+train_dat$fraud <- as.numeric(train_dat$fraud)
 
 # Train the model on the training set
-my.grid <- expand.grid(interaction.depth = 1:2,
-                       shrinkage = .1,
-                       n.trees = c(2000,3000,4000),
-                       n.minobsinnode = 10)
+# my.grid <- expand.grid(interaction.depth = 1:2,
+#                        shrinkage = .1,
+#                        n.trees = c(2000,3000,4000),
+#                        n.minobsinnode = 10)
 
-my.control <- trainControl(method = 'cv',
-                           number = 10,
-                           returnResamp = "all",
-                           classProbs = TRUE,
-                           summaryFunction = twoClassSummary)
-
+my.control <- trainControl(method = 'cv', number = 10)
+set.seed(1234)
 bt.fit <- train(fraud ~ e.1meanamount_zip3 + e.7medianamount_zip3 + e.1medianamount_zip3 + e.7meanamount_cardnum +
                     e.7avgdailyfreq_cardnum + e.7meanamount_merchnum + e.7avgdailyfreq_merchnum + e.1countzip3_cardnum +
                     e.curdaybal_dow_cardnum + e.curdaybal_dow_merchnum,
                 data = train_dat,
+                distribution = "gaussian",
                 method = "gbm",
                 trControl = my.control,
-                tuneGrid = my.grid,
+                # tuneGrid = my.grid,
                 preProc = c("center", "scale"),
-                metric = "ROC",
+                metric = "RMSE",
                 verbose = FALSE,
                 distribution = "adaboost")
 
