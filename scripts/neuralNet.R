33df0282e17182d7e7de3577a98976e8ee032be4
Bryce Charette <bryce.charette@gmail.com>
2017-08-01 15:01:32 -0700

    Updated neural net script

--- scripts/neuralNet.R
+++ scripts/neuralNet.R
@@ -7,10 +7,6 @@ train_dat <- readRDS("data/train.RDS")
 # Set seed
 set.seed(1234)
 
-e.1meanamount_zip3 + e.7medianamount_zip3 + e.1medianamount_zip3 + e.7meanamount_cardnum +
-e.7avgdailyfreq_cardnum + e.7meanamount_merchnum + e.7avgdailyfreq_merchnum + e.1countzip3_cardnum +
-  e.curdaybal_dow_cardnum + e.curdaybal_dow_merchnum
-
 # Train the model on the training set
 my.grid <- expand.grid(.decay = c(0.1,0.5,0.9), .size = c(2, 3, 4))
 my.control <- trainControl(method = 'cv', number = 10)
