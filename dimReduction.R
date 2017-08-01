713894b2aa669c1d14abc4574ed4890fe3b76d5c
joshuabhatnagar <joshuabhatnagar@users.noreply.github.com>
2017-08-01 10:39:09 -0700

    ks results and dim reduction script

--- dimReduction.R
+++ dimReduction.R
@@ -1,8 +1,10 @@
 # read in data
-dat_df <- readRDS("data/expert_card_payments.RDS")
+dat_df <- readRDS("data/expert_card_payments_v3.RDS")
 
 # reduce data set to only expert variables
-dat_df <- dat_df[,-c(1:9,11)]
+dat_df <- dat_df[ ,!(names(dat_df) %in% c("merch_zip", "merch_description", "merchnum",
+                                          "record_number", "cardnum", "date", "merch_state",
+                                          "transtype", "amount", "merch_zip3", "dow"))]
 
 #### UNIVARIATE KS ####
 #install.packages("ks")
@@ -13,7 +15,7 @@ ks.results <- data.frame(variables = colnames(dat_df)[2:length(dat_df)]) %>%
   mutate(D.stat = vector(mode = "numeric", length = (length(.))))
 
 # univariate test each expert variable --- NOT WORKING
-for(i in 1:length(ks.results)){
+for(i in 1:nrow(ks.results)){
   good <- filter(.data = dat_df, fraud==FALSE) %>%
     select(colnames(.)[i+1])
   bad <- filter(.data = dat_df, fraud==TRUE) %>%
@@ -24,6 +26,9 @@ for(i in 1:length(ks.results)){
 
 # arrange variables according to D-statistic
 ks.results <- ks.results %>% arrange(D.stat)
+
+# save KS results to RDS file
+saveRDS(ks.results, "data/KSresults.RDS")
 
 # find the top 85% of variables based on D-stat
 n <- 85
